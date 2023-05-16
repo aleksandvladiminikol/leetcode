4c9d336d3165e12ae3feb8c18d135dfca1b8c606
Aleksandr Nikolaev <aleksandr.nikolaev@ihsmarkit.com>
2023-05-16 21:59:31 +0530

    Two sum final

--- 2.TwoSum/python/solution.py
+++ 2.TwoSum/python/solution.py
@@ -1,13 +1,11 @@
 from typing import List
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for index, num in enumerate(nums):
+        num_set = set()
+        for i, num in enumerate(nums):
             remaining = target - num
-            try:
-                index_0 = nums.index(remaining)
-            except ValueError:
-                continue
-            if index_0 == index:
-                continue
-            return (index, index_0)
-
+            if remaining in num_set:
+                return [i, nums.index(remaining)]
+            num_set.add(num)
+        return []
